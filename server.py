852e3b72cb0fd2a8e8eb06022a39023abda7eb2d
Devin Bayly <baylyd@email.arizona.edu>
2019-11-23 11:53:36 -0700

    add support for multiple tns on server

--- server.py
+++ server.py
@@ -5,19 +5,32 @@ import telnetlib
 import threading
 import re
 
-host = "localhost"
-port = "4000"
-tn = telnetlib.Telnet(host, port)
+# so we actually have to store a list of telnets, and when we start, let the first message be an identifying message sent to the client, that it must preface all other ones with
+# connection is made, echo is called message is "handshake", tn is created, and pushed into stack, and readloop threadstarted ws responds to the client with a ID number that must go in front of other messages,then that index is used to find the correct tn session and comm with it
+# must add supprt to decipher which client should respond to the message in the onmessage part
 
 
 class TelSock:
-    def __init__(self, tn):
-        self.tn = tn
+    def __init__(self):
+        self.host = "localhost"
+        self.port = "4000"
+        self.thread_up = False
+        self.tns = []
 
-    async def echo(self, websocket, path):
+   async def echo(self, websocket, path):
         self.ws = websocket
-        threading.Thread(target=self.start_read_eventloop).start()
+        # sending ws to tn
+
         async for message in websocket:
+            if "register" in message[:len("register")]:
+                # initialize the tn
+                temp_tn = telnetlib.Telnet(self.host,self.port)
+                self.tns.append(temp_tn)
+                if not self.thread_up:
+                    threading.Thread(target=self.start_read_eventloop).start()
+                    self.thread_up = True
+                # write back the id
+                await self.ws.send("id"+len(self.tns))
             print(message)
             self.tn.write("{}\n".format(message).encode())
 
@@ -28,19 +41,23 @@ class TelSock:
     async def read_loop(self):
         print("in readloop")
         print(self.ws)
+        # sending tn to ws
         while True:
-            contents = tn.read_until(b"\n", .5).decode("utf-8")
-            if not contents == "":
-                ansi_escape = re.compile(r'''
-                    \x1B    # ESC
-                    [@-_]   # 7-bit C1 Fe
-                    [0-?]*  # Parameter bytes
-                    [ -/]*  # Intermediate bytes
-                    [@-~]   # Final byte
-                ''', re.VERBOSE)
-                result = ansi_escape.sub('', contents)
-                print(result)
-                await self.ws.send(result)
+            ## go over the tns we have to listen on
+            if self.thread_up:
+            for tn in self.tns:
+                contents = tn.read_until(b"\n", .2).decode("utf-8")
+                if not contents == "":
+                    ansi_escape = re.compile(r'''
+                        \x1B    # ESC
+                        [@-_]   # 7-bit C1 Fe
+                        [0-?]*  # Parameter bytes
+                        [ -/]*  # Intermediate bytes
+                        [@-~]   # Final byte
+                    ''', re.VERBOSE)
+                    result = ansi_escape.sub('', contents)
+                    print(result)
+                    await self.ws.send(result)
 
 
 ts = TelSock(tn)
